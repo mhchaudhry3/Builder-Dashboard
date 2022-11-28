abcffddeee696823bf702d416a2a65ff4b5a1f7f
mhchaudhry3 <mobeen@blocknative.com>
2022-11-28 17:22:44 -0500

    WIP Second bid not displaying

---
+++ components/columns.js
@@ -0,0 +1,40 @@
+export const columns = [
+    {
+        name: 'Slot',
+        selector: row => row.slot,
+        width: '100px',
+        cell: (row) => <a link="google.com">{row.slot}</a>
+        // cell: row => <a href="url">link text</a>
+        // <link>https://beaconcha.in/block/{row.slot}</link>
+    },
+    {
+        name: 'Builder Address',
+        selector: row => row.builder_pubkey,
+        width: '100px'
+    },
+    {
+        name: 'Block Hash',
+        selector: row => row.block_hash,
+        width: 'auto-fit',
+        maxWidth: '550px'
+
+    },
+    {
+        name: 'Second Highest Bid',
+        selector: row => row.secondHighestBid,
+        width: '200px',
+        // cell: row => (<div>{row.secondHighestBid}</div>)
+        cell: row => (<div>{console.log(row)}</div>)
+    },
+    {
+        name: 'Value',
+        selector: row => row.value,
+        width: '200px',
+        cell: row => (<div>{(row.value/(10**18)).toFixed(5)}</div>)
+    },
+    // {
+    //     name: 'longitude',
+    //     selector: row => row.longitude,
+    //     width: '100px'
+    // },
+  ];
---
+++ pages/api/relayApis.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import NextCors from 'nextjs-cors';
+import { NextApiRequest, NextApiResponse } from 'next'
+
+
+
+export default function handler(req, res) {
+  // const data = blocknativeRelayApi()
+//   NextCors(req, res, {
+//     // Options
+//     methods: ['Access-Control-Allow-Methods','GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+//     origin: '*',
+//     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// //  });
+//   axios.get(`https://boost-relay.flashbots.net/relay/v1/data/bidtraces/builder_blocks_received?slot=5197341`)
+//   .then(response => res.status(200).json(response))
+  // res.status(200).json(data)
+  }
