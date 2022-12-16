e3cc838dfa7832afc9f424d14a79a882798533ce
mhchaudhry3 <mobeen@blocknative.com>
2022-12-16 12:55:52 -0500

    UPDATES

---
+++ components/styles.js
@@ -0,0 +1,35 @@
+export const titleStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "20px",
+    backgroundColor: "white",
+    color: "black",
+  };
+
+export const tableStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "20px",
+    backgroundColor: "white",
+  };
+
+  export  const accountDataBox = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    fontSize: "18px",
+    backgroundColor: "white",
+    rowHeight: "2rem",
+    color: "black",
+    borderStyle: "groove",
+    paddingBottom: ".5rem",
+  };
+
+  export const TitleHeader = {
+    borderColor: "black",
+    paddingBottom: ".5rem",
+  };
+
+  export const RowStyle = { fontSize: "14px" };
