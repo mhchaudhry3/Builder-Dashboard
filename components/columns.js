6af17f9c159772e23ee6874e872b806eeb5d3468
mhchaudhry3 <mobeen@blocknative.com>
2022-12-22 18:27:47 -0500

    bid efficiency

--- components/columns.js
+++ components/columns.js
@@ -57,4 +57,33 @@ export const columns = [
       </div>
     ),
   },
+  {
+    name: "Bid Efficiency",
+    width: "105px",
+    cell: (row) =>
+      calculateEfficiency(row.value, row.secondHighBid, row.builder_pubkey),
+  },
 ];
+
+const calculateEfficiency = (winningBid, losingBid, pubkey) => {
+  if (
+    pubkey ===
+    "0x8000008a03ebae7d8ab2f66659bd719a698b2e74097d1e423df85e0d58571140527c15052a36c19878018aaebe8a6fea"
+  ) {
+    const profit = (winningBid / 10 ** 18) * 0.05;
+    const blockProfitPlusValidator = winningBid / 10 ** 18 + profit;
+    const differenceOfWinningBid =
+      blockProfitPlusValidator - losingBid / 10 ** 18;
+    return ((profit / differenceOfWinningBid) * 100).toFixed(2) + "%";
+  } else if (
+    pubkey ===
+    "0x9000009807ed12c1f08bf4e81c6da3ba8e3fc3d953898ce0102433094e5f22f21102ec057841fcb81978ed1ea0fa8246"
+  ) {
+    const profit = (winningBid / 10 ** 18) * 0.001;
+    const blockProfitPlusValidator = winningBid / 10 ** 18 + profit;
+    const differenceOfWinningBid =
+      blockProfitPlusValidator - losingBid / 10 ** 18;
+    const blockProfit = winningBid * 0.05;
+    return ((profit / differenceOfWinningBid) * 100).toFixed(2) + "%";
+  }
+};
